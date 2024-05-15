844966119fbec1ea546d762fb4e03fe2a5d570ad
parsapanahpoor <parsapanahpoor77@gmail.com>
2024-05-15 13:16:37 +0330

    Add raw layers and dlls

---
+++ ClinicSalamat.MVC/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ClinicSalamat.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
